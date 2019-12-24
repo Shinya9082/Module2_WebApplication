4603c51b37d380960a68e72ab3fa6d847074c1ed
Shinya9082 <anhtuandtu1993@gmail.com>
2019-12-24 14:57:40 +0700

    dome hit counter

---
+++ session_cookie/demo_hit_counter/src/test/java/com/codegym/demo_hit_counter/DemoHitCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.demo_hit_counter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoHitCounterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
